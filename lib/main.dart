e4c8dd421d2a3f9d74b773d94d92fa7105a891b1
hob21081973-glitch <hob21081973@gmail.com>
2026-09-20 19:01:06 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,10 @@ const String urlClientesCSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1v
 const String urlProductosCSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtKhEE5ziDtm_BQdAeOy8c-Z6H6_GbyKcPOvtdjfKtXgxYObBUB-PlK0ldsiwrW78aabDzei-R2Cd/pub?gid=1903712481&single=true&output=csv';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const AppVentasExportPdf());
+  runApp(const AppVentasHob());
 }
-class AppVentasExportPdf extends StatelessWidget {
-  const AppVentasExportPdf({super.key});
+class AppVentasHob extends StatelessWidget {
+  const AppVentasHob({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
