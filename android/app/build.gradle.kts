dc407172f3d0120488b16bd8f2aab331c8090bfd
hob21081973-glitch <hob21081973@gmail.com>
2026-09-22 21:56:05 -0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_ventas_export_pdf"
+    namespace = "com.example.app_Ventas_Hob"
     compileSdk = 34
 
     compileOptions {
@@ -18,7 +18,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.app_ventas_export_pdf"
+        applicationId = "com.example.app_Ventas_Hob"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
